8182c2c0aa5598f79c05b3ce918ab10e46b4b881
sunchenlong <chenlong.sun@haiziwang.com>
2021-08-09 15:42:59 +0800

    增加解释&logo

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,8 @@
 #! /usr/bin/env node
 
 const { program } = require('commander')
-
+const chalk = require('chalk')
+const figlet = require('figlet')
 
 program
   .command('create <app-name>')
@@ -11,10 +12,42 @@ program
     require('../lib/create')(name, options)
   })
 
+// 配置config命令
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  })
+
+// 配置UI命令
+program
+  .command('ui')
+  .description('start add open roc-cli ui')
+  .option('-p, --port <port> ', 'delete used for the UI server')
+  .action((value, options) => {
+    console.log(value, options)
+  })
 
 program
   .version(`v${require('../package.json').version}`)
   .usage('<command> [option]')
 
+
+program.on('--help', () => {
+
+  console.log('\r\n' + figlet.textSync('yuanlong', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 120,
+    whitespaceBreak: true
+  }));
+
+  console.log(`\r\nRun ${chalk.cyan(`YL <command> --help`)} for detailed usage of given command\r\n`)
+})
 
 program.parse(process.argv)
